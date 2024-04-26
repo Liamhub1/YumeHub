2b5bc039784e12bc174df6bcc78998baa65d1ed4
Liamhub1 <155800591+Liamhub1@users.noreply.github.com>
2024-04-26 07:59:54 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,7 @@
-local configi = loadstring(game:HttpGet(("https://raw.githubusercontent.com/Liamhub1/YumeHub/main/Config.lua"), true))()
+local configi = loadstring((game:HttpGet("https://raw.githubusercontent.com/Liamhub1/YumeHub/main/obf_Q6SGnDAyB4LxV3992tvmQJnFp06b8bZ4LhPF1s41f6UkKA99aiDrG4HlSmfw7O1c.lua.txt"), true))()
 
 if game.PlaceId == 3101667897 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Liamhub1/YumeHub/main/Speed%20of%20Legends.lua", true))()
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Liamhub1/YumeHub/main/Speed%20of%20Legends.lua"), true))()	
 elseif game.PlaceId == 15509350986 then
     loadstring(game:HttpGet(("https://raw.githubusercontent.com/Liamhub1/YumeHub/main/YumeHub.lua"), true))()
 elseif game.PlaceId ~= 13772394625 and game.PlaceId ~= 15234596844 and game.PlaceId ~= 14732610803 and game.PlaceId ~= 13822889 and game.PlaceId ~= 142823291 then
